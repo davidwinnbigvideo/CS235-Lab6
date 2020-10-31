aa7aeb7a90a56979bea751ddb422b8269ad4fb2f
bigvideo <dwinn@byu.net>
2020-10-31 23:44:36 +0000

    finish v1

--- LinkedList.h
+++ LinkedList.h
@@ -13,12 +13,12 @@ private:
 
   struct Node {
     T data; // The data we are storing
-    Node* next; // A pinter to the next node
+    Node* next; // A pointer to the next node
     Node(const T& the_data, Node* next_val = NULL) :
       data(the_data) {next = next_val;}
   };
 
-  Node *mylist;
+  Node *mylist, *head, *tail;
   int num_items;
 
 public:
@@ -37,9 +37,13 @@ public:
 	Do not allow duplicate values in the list.
 	*/
 	virtual void insertHead(T value) {
+    if (!checkDuplicates(value)) {
     Node *ptr = mylist;
     mylist = new Node(value);
     mylist->next = ptr;
+    num_items++;
+    return;
+    }
   };
 
 	/*
@@ -50,10 +54,13 @@ public:
 	Do not allow duplicate values in the list.
 	*/
 	virtual void insertTail(T value){
+    if (!checkDuplicates(value)) {
     Node *ptr = mylist;
     mylist = new Node(value);
     mylist->next = ptr;
-
+    num_items++;
+    return;
+    }
   };
 
 	/*
@@ -65,8 +72,35 @@ public:
 	A node should only be added if the node whose value is equal to
 	insertionNode is in the list. Do not allow duplicate values in the list.
 	*/
-	virtual void insertAfter(T value, T insertionNode){
-
+	virtual void insertAfter(T value, T insertionNode) {
+    if (num_items == 0) {
+      return;
+    }
+    Node *currentNode = head;
+    if (!checkDuplicates(value)) {
+      while (currentNode->next != NULL) {
+        if (currentNode->next != NULL) {
+          if (currentNode->value == insertionNode) {
+            Node *ptr = mylist;
+            mylist = new Node(value);
+            mylist->next = currentNode->next;
+            currentNode->next = mylist;
+            num_items++;
+            return;
+          }
+          currentNode = currentNode->next;
+        }
+        if (currentNode == tail && currentNode->value == insertionNode) {
+          Node *ptr = mylist;
+          mylist = new Node(value);
+          tail->next = mylist;
+          mylist->next = NULL;
+          tail = mylist;
+          num_items++;
+          return;
+        }
+      }
+    }
   };
 
 	/*
@@ -76,8 +110,44 @@ public:
 
 	The list may or may not include a node with the given value.
 	*/
-	virtual void remove(T value){
-
+	virtual void remove(T value) {
+    node *currentNode = head;
+    node *previousNode = NULL;
+    if (num_items < 1) {
+      return;
+    }
+    if (checkDuplicates(value)) {
+      while (currentNode->next != NULL) {
+        if (currentNode->value == value) {
+          if (head == currentNode) {
+            head = head->next;
+            delete currentNode;
+            num_items--;
+            return;
+          } else {
+            previousNode->next = currentNode->next;
+            currentNode->next = NULL;
+            delete currentNode;
+            num_items--;
+            return;
+          }
+        }
+        previousNode = currentNode;
+        currentNode = currentNode->next;
+      }
+      if (num_items == 1) {
+        previousNode = currentNode;
+      }
+      tail = previousNode;
+      tail->next = NULL;
+      delete currentNode;
+      num_items--;
+      if (num_items == 0) {
+        head = NULL;
+        tail = NULL;
+      }
+      return;
+    }
   };
 
 	/*
@@ -85,8 +155,19 @@ public:
 
 	Remove all nodes from the list.
 	*/
-	virtual void clear(){
-
+	virtual void clear() {
+    Node *currentNode;
+    Node *previousNode;
+    currentNode = head;
+    while (currentNode->next != NULL) {
+      previousNode = currentNode;
+      currentNode = currentNode->next;
+      delete previousNode;
+    }
+    delete currentNode;
+    head = NULL;
+    tail = NULL;
+    num_items = 0;
   };
 
 	/*
@@ -97,8 +178,15 @@ public:
 
 	If the given index is out of range of the list, throw an out of range exception.
 	*/
-	virtual T at(int index){
-
+	virtual T at(int index) {
+    if(index >= num_items) {
+			throw std::out_of_range("At Error");
+		} else {
+			for (int i = 0; i < index; i++) {
+        currentNode = currentNode->next;
+      }
+      return currentNode->value;
+		}
   };
 
 	/*
@@ -106,8 +194,8 @@ public:
 
 	Returns the number of nodes in the list.
 	*/
-	virtual int size(){
-
+	virtual int size() {
+    return num_items;
   };
 
 	/*
@@ -119,12 +207,23 @@ public:
 	For example, a LinkedList containing the value 1, 2, 3, 4, and 5 should return
 	"1 2 3 4 5"
 	*/
-	virtual string toString(){
+	virtual string toString() {
     stringstream ss;
     for(Node *ptr = mylist; ptr != NULL; ptr = ptr->next){
       ss << "ptr " << ptr << " val " << ptr->data << " next " << ptr-> next << endl;
     }
     return (ss.str());
   };
+
+  bool checkDuplicates(T value) {
+    Node *currentNode = head;
+    while (currentNode->value != value) {
+      if (currentNode->next == NULL) {
+        return false;
+      }
+      currentNode = currentNode->next;
+    }
+    return true;
+  }
 
 };
